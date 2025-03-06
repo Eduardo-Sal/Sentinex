f4859968a8ee71390bc005f2101d183727a8ed23
Eduardo-Sal <109422239+Eduardo-Sal@users.noreply.github.com>
2025-03-06 21:51:52 +0000

    added one image route

---
+++ Sentinex Backend/app/images.py
@@ -0,0 +1,77 @@
+# same as clips.py except with screenshots
+# handles screenshot sent from robot & metadata
+
+from fastapi import APIRouter, UploadFile,File, Form
+import boto3
+import uuid
+from datetime import datetime
+from config import cognito, connect_db, app_client_id, s3_bucket, s3, aws_region, user_pool_id
+
+images_router = APIRouter()
+
+# uploads screenshot to s3 and metadata in dynamo
+def storeUserImage(user_id:str, image):
+    """
+    Stores image into S3 bucket associated with User_ID
+    """
+    try:
+        image_decoded = image.file.read()
+
+        filePath = f"{user_id}/screenshot/{image.filename}"
+
+        s3.put_object(
+            Bucket = s3_bucket,
+            Key = filePath,
+            Body = image_decoded,
+            ContentType = image.content_type
+        )
+        print(f"Stored Image at s3://{s3_bucket}/{filePath}")
+    except Exception as e:
+        print(f"Failed to store image {e}")
+
+@images_router.post("/upload")
+def upload_screenshot(robot_id: str  = Form(...), image: UploadFile = File(...)):
+    conn = connect_db()
+    cursor = conn.cursor()
+    try:
+        # get user_id
+        cursor.execute("SELECT user_id FROM robots where id=%s",(robot_id,))
+        user_id = cursor.fetchone()
+        
+        #handle error
+        if not user_id:
+            print("No user ID associated with the robot_id, but that's impossible how did the robot do this request without a user?!?!")
+            return None
+
+        # get val from tuple
+        user_id = user_id[0]
+
+        # get user_uuid
+        cursor.execute("SELECT cognito_sub FROM users where id=%s",(user_id,))
+        user_uuid = cursor.fetchone()
+
+        # handle error
+        if not user_uuid:
+            print("Doesn't exist")
+            return None
+        user_uuid = user_uuid[0]
+
+        storeUserImage(user_uuid, image)
+
+        return {"message": "Image uploaded successfully", "user_uuid": user_uuid}
+
+    except Exception as e:
+        print(f"Failed to upload image {e}")
+    
+
+# for returning presigned url for screenshot of the robot for viewing 
+@images_router.get("/{screenshot_id}/url")
+def get_presigned_url(screenshot_id: str):
+    pass
+
+# obtain all the alerts related to the robot_id paired with the user so Users table will have robot_id or try with user_id
+@images_router.get("/get-alerts/{user_id}")
+def getAllalerts(screenshot_id: str):
+    '''Gets all alerts from the Database associated with the user_id '''
+
+    
--- Sentinex Backend/app/main.py
+++ Sentinex Backend/app/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from auth import auth_router
 from robots import robots_router
-#from screenshots import router as screenshots_router
+from screenshots import images_router
 #from clips import router as clips_router
 #from stream import router as stream_router
 import uvicorn 
@@ -13,7 +13,7 @@ app = FastAPI()
 # Register API Routes for backend
 app.include_router(auth_router, prefix="/api/auth")
 app.include_router(robots_router, prefix="/api/robots")
-#app.include_router(screenshots_router, prefix="/api/screenshots")
+app.include_router(screenshots_router, prefix="/api/images")
 #app.include_router(clips_router, prefix="/api/clips")
 #app.include_router(stream_router, prefix="/api/stream")
 
